c64998c11cfa7611e210fd2a465ef3fca589cf6d
Dawid Laszuk <11457963+laszukdawid@users.noreply.github.com>
2025-04-18 10:14:35 -0700

    Gemini connector (#4)

--- internal/tools/main.go
+++ internal/tools/main.go
@@ -2,12 +2,15 @@ package tools
 
 func GetAllTools() map[string]Tool {
 	tools := map[string]Tool{
-		NewUnixTool(nil).Name():   NewUnixTool(nil),
-		NewWebsearchTool().Name(): NewWebsearchTool(),
+		NewUnixTool(nil).Name(): NewUnixTool(nil),
 		// NewPythonTool().Name():       NewPythonTool(),
 		// NewPythonToolWithContext().Name(): NewPythonToolWithContext(),
 	}
 
+	// Add Websearch only if possible
+	if websearchTool := NewWebsearchTool(); websearchTool != nil {
+		tools[websearchTool.Name()] = websearchTool
+	}
 	return tools
 }
 
